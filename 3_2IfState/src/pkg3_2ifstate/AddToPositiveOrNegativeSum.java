c08f310e803de725454460c5f4ffc2c5df50a734
jbeckenbach <jbeckenbach@providencehall.com>
2019-11-01 08:30:40 -0600

    Add files via upload

---
+++ 3_2IfState/src/pkg3_2ifstate/AddToPositiveOrNegativeSum.java
@@ -0,0 +1,30 @@
+/*
+ * Write a program that will allow the user to input an int.  If the number is negative, 
+ * add the number to a variable to store a negative sum. If the number is positive, 
+ * add the number to a different variable to store a positive sum. 
+ * At the conclusion of the program, output the sums.  
+ * 
+ * Use this starter file and enter your code segments in the commented areas 
+ * that describe the given tasks. 
+ */
+
+import java.util.Scanner;
+
+public class AddToPositiveOrNegativeSum
+{
+    public static void main(String[] args)
+    {
+        //declare and instantiate a Scanner variable and object
+        
+        //declare and initialize a positive and negative sum
+        
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            // in this block, allow the user to input a number and add the number
+            // to the appropriate sum
+        }
+        
+        //output the sums
+    }
+}
---
+++ 3_2IfState/src/pkg3_2ifstate/TestThatNumber.java
@@ -0,0 +1,20 @@
+/*
+ * Write a program that will allow the user to enter an 
+ * int.  Using if statements, test the following and print out the following:
+ * 1.  if the number is positive
+ * 2.  if the number is 0
+ * 3.  if the number is negative
+ * 4.  if the number is nonnegative
+ * 5.  if the number is nonpositive
+ * 6.  if the number is even
+ * 7.  if the number is odd
+ * 8.  if the number is divisible by 10
+ * 9.   if the number is not divisible by 10
+ */
+
+public class TestThatNumber
+{
+    public static void main(String[] args)
+    {
+    }
+}
